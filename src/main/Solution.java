6f62d21cfcb85ec5657e142f00f92eaf19ae939a
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-09-19 18:15:59 +0530

    taking an input

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         final Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
     }
 
 
