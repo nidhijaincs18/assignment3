04b898ec4100a7b291351fedd7a574693de4fb9e
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-09-19 21:06:24 +0530

    create a defination class

---
+++ src/main/Book.java
@@ -0,0 +1,47 @@
+package main;
+
+public class Book {
+    private String book;
+    private String author;
+    private String isbn;
+
+    public Book(String book, String author, String isbn) {
+        this.book = book;
+        this.author = author;
+        this.isbn = isbn;
+    }
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("-----------------------------\n");
+        result.append("Book Name:\t" + getBook() + "\nAuthor Name:\t" + getAuthor() + "\nISBN:\t" + getIsbn() + "\n");
+        result.append("-----------------------------");
+        return result.toString();
+
+    }
+}
+
--- src/main/Solution.java
+++ src/main/Solution.java
@@ -2,9 +2,10 @@ package main;
 
 import java.util.Scanner;
 
+
 public class Solution {
     public static void main(String[] args) {
-        final Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
 
         if (n == 0) {
@@ -21,56 +22,10 @@ public class Solution {
             for (int i = 0; i < books.length; i++) {
                 System.out.println(books[i]);
             }
-    }
+        }
         s.close();
 
 
-}
-
-
-class Book {
-    private String book;
-    private String author;
-    private String isbn;
-
-    public Book(String book, String author, String isbn) {
-        this.book = book;
-        this.author = author;
-        this.isbn = isbn;
-    }
-
-    public String getBook() {
-        return book;
-    }
-
-    public void setBook(String book) {
-        this.book = book;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public String toString() {
-        StringBuilder result = new StringBuilder();
-        result.append("-----------------------------\n");
-        result.append("Book Name:\t" + getBook() + "\nAuthor Name:\t" + getAuthor() + "\nISBN:\t" + getIsbn() + "\n");
-        result.append("-----------------------------");
-        return result.toString();
-
     }
 }
 
-}
