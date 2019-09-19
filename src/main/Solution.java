41cf8e5ae8bdd333405c7047dde2a06a442468a5
nidhijaincs18 <nidhi.jain_cs18@gla.ac.in>
2019-09-19 18:16:55 +0530

    checks the condition whether n is 0 or not if it is zero then it will print 'N/A'

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -6,6 +6,10 @@ public class Solution {
     public static void main(String[] args) {
         final Scanner s = new Scanner(System.in);
         int n = s.nextInt();
+
+        if (n == 0) {
+            System.out.println("N/A");
+        }
     }
 
 
